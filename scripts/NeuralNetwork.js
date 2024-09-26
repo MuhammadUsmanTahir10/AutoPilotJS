c8931088df824b837e7bc344100b7868b55fd1db
M Usman Tahir <44548891+MuhammadUsmanTahir10@users.noreply.github.com>
2024-09-26 23:13:33 +0500

    Create NeuralNetwork.js

---
+++ scripts/NeuralNetwork.js
@@ -0,0 +1,41 @@
+// Neural Network class for decision-making
+class NeuralNetwork {
+    constructor(neuronCounts) {
+        this.levels = [];
+        for (let i = 0; i < neuronCounts.length - 1; i++) {
+            this.levels.push(new Level(neuronCounts[i], neuronCounts[i + 1]));
+        }
+    }
+
+    static feedForward(givenInputs, network) {
+        let outputs = Level.feedForward(givenInputs, network.levels[0]);
+        for (let i = 1; i < network.levels.length; i++) {
+            outputs = Level.feedForward(outputs, network.levels[i]);
+        }
+        return outputs;
+    }
+}
+
+class Level {
+    constructor(inputCount, outputCount) {
+        this.inputs = new Array(inputCount);
+        this.outputs = new Array(outputCount);
+        this.biases = new Array(outputCount);
+
+        this.weights = [];
+        for (let i = 0; i < inputCount; i++) {
+            this.weights[i] = new Array(outputCount);
+        }
+
+        Level.#randomize(this);
+    }
+
+    static #randomize(level) {
+        // Randomizes biases and weights
+    }
+
+    static feedForward(givenInputs, level) {
+        // Forward pass through the level
+        return [];
+    }
+}
