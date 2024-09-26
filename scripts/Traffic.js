309b074b5ffb63d05f4ae2fcd32241a116e76b2d
M Usman Tahir <44548891+MuhammadUsmanTahir10@users.noreply.github.com>
2024-09-26 23:24:04 +0500

    Create Traffic.js

---
+++ scripts/Traffic.js
@@ -0,0 +1,14 @@
+// Traffic class to simulate other vehicles on the road
+class Traffic {
+    constructor() {
+        // Traffic setup
+    }
+
+    update() {
+        // Update traffic state
+    }
+
+    draw(ctx) {
+        // Draw traffic vehicles
+    }
+}
